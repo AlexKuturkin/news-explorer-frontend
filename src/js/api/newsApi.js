175b4a216a219c4d722d18d31d96333f06d6ac48
Alex Kuturkin <alexkuturkin@mail.ru>
2020-05-23 10:14:25 +0300

    fix newsapi

--- src/js/api/newsApi.js
+++ src/js/api/newsApi.js
@@ -3,7 +3,7 @@ import dateRequest from "../utils/dateRequest";
 export default class NewsApi {
   getNews(keyword) {
     return fetch(
-      `https://newsapi.org/v2/everything/?q=${keyword}${dateRequest()}&pageSize=100&apiKey=4efe7932809d4f688579972bc220bc86`,
+      `https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/everything/?q=${keyword}${dateRequest()}&pageSize=100&apiKey=4efe7932809d4f688579972bc220bc86`,
       {
         method: "GET",
       }
